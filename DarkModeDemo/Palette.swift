85921f3e542d8d64dcbadee2c2c33c108ff42a49
iMoe <iMoeNya@outlook.com>
2019-09-22 02:52:06 +0800

    Remove demo code

--- DarkModeDemo/Palette.swift
+++ DarkModeDemo/Palette.swift
@@ -10,28 +10,18 @@ import UIKit
 
 extension UIColor {
     enum text {
-//        static let tint = UIColor(red: 254/255, green: 139/255, blue: 164/255, alpha: 1)
-//        static var tint: UIColor {
-//            let `default` = UIColor(red: 254/255, green: 139/255, blue: 164/255, alpha: 1)
-//            if #available(iOS 13.0, *) {
-//                return .init { (traitCollection) -> UIColor in
-//                    if traitCollection.userInterfaceStyle == .dark {
-//                        return UIColor(red: 39/255, green: 143/255, blue: 168/255, alpha: 1)
-//                    } else {
-//                        return `default`
-//                    }
-//                }
-//            } else {
-//                return `default`
-//            }
-//        }
         static var tint: UIColor {
-            return .init { (traitCollection) -> UIColor in
-                if traitCollection.userInterfaceStyle == .dark {
-                    return UIColor(red: 39/255, green: 143/255, blue: 168/255, alpha: 1)
-                } else {
-                    return UIColor(red: 254/255, green: 139/255, blue: 164/255, alpha: 1)
+            let `default` = UIColor(red: 254/255, green: 139/255, blue: 164/255, alpha: 1)
+            if #available(iOS 13.0, *) {
+                return .init { (traitCollection) -> UIColor in
+                    if traitCollection.userInterfaceStyle == .dark {
+                        return UIColor(red: 39/255, green: 143/255, blue: 168/255, alpha: 1)
+                    } else {
+                        return `default`
+                    }
                 }
+            } else {
+                return `default`
             }
         }
     }
