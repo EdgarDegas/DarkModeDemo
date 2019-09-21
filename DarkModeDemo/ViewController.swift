6bb7d19470c4a8ccb787f4d2387a65823fe6f98a
iMoe <iMoeNya@outlook.com>
2019-09-22 00:23:21 +0800

    Initial Commit

---
+++ DarkModeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkModeDemo
+//
+//  Created by iMoe on 2019/9/22.
+//  Copyright © 2019 imoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
